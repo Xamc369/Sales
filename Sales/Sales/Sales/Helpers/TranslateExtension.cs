a3a96cd4265acb8e94194909680c8ca3cbb9712a
Xamc369 <61982770+Xamc369@users.noreply.github.com>
2020-03-16 15:23:04 -0500

    multil

--- Sales/Sales/Sales/Helpers/TranslateExtension.cs
+++ Sales/Sales/Sales/Helpers/TranslateExtension.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Sales.Helpers
+﻿namespace Sales.Helpers
 {
-    class TranslateExtension
+    using System;
+    using System.Globalization;
+    using System.Reflection;
+    using System.Resources;
+    using Interfaces;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [ContentProperty("Text")]
+
+    public class TranslateExtension : IMarkupExtension
     {
+        readonly CultureInfo ci;
+        const string ResourceId = "Sales.Resources.Resource";
+
+        static readonly Lazy<ResourceManager> ResMgr =
+            new Lazy<ResourceManager>(() => new ResourceManager(
+                ResourceId,
+                typeof(TranslateExtension).GetTypeInfo().Assembly));
+
+        public TranslateExtension()
+        {
+            ci = DependencyService.Get<ILocalize>().GetCultureInfo();
+
+        }
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        { 
+            if(Text == null)
+            {
+                return "";
+            }
+        var translation = ResMgr.Value.GetString(Text, ci);
+
+        if(translation == null)
+        {
+#if DEBUG
+        throw new ArgumentException(
+                           String.Format("Key '{0}' was not ofund in resources '{1}' for culture '{2}'.",
+                           Text, ResourceId, ci.Name), "Text");
+#else
+        translation = Text;
+#endif
+        }
+        return translation;
+        }
     }
 }
